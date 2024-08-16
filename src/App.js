ac1d04c1c451f77fb05edca7e0a0cf7ad2ef0ba0
Michael Gattas <michael.gattas@stjude.org>
2024-08-16 16:07:31 -0500

    minor updates with travis file and prod/dev Dockerfiles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YO YO YO whaddup!
+          YO YO YO waddo!
         </p>
         <a
           className="App-link"
