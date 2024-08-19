51e65c8b3b15f8a8b51b5424b570222a40e0c098
Michael Gattas <michael.gattas@stjude.org>
2024-08-19 14:52:20 -0500

    updates test again

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders try again', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/try again/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders try again', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/try again/i);
   expect(linkElement).toBeInTheDocument();
 });
