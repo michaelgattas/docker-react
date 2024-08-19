313986de536fb21151104f354c87681a3afb6758
Michael Gattas <michael.gattas@stjude.org>
2024-08-19 14:44:14 -0500

    minor change to retrigger Travis Deployment to AWS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's try again?
         </a>
       </header>
     </div>
